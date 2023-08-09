c0cc56067b14b5d60a3008b52f7e9f1206df6b5c
Angela Shen <angelayshen@gmail.com>
2023-08-09 00:21:49 -0400

    update Story Page

--- client/src/components/StoryPage.js
+++ client/src/components/StoryPage.js
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 function StoryPage() {
   const [story, setStory] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const { id } = useParams();
 
   useEffect(() => {
     fetch(`/stories/${id}`)
-      .then((response) => response.json())
-      .then((data) => setStory(data));
-  }, [id]); // Include id as a dependency
+      .then((response) => {
+        if (response.status === 404) {
+          setStory(null);
+          setLoading(false);
+          return;
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data) { // Check if data exists
+          setStory(data);
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        // Handle any fetch error here
+        setLoading(false);
+      });
+  }, [id]);
+  
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
@@ -20,10 +42,14 @@ function StoryPage() {
           <pre className="generated-content">{story.content}</pre>
         </>
       ) : (
-        <p>Loading...</p>
+        <p>Oops, this story doesn't exist yet!</p>
       )}
+      <p className="generated-content-type" style={{ color: 'white' }}>
+        Want to generate your own story? &nbsp;
+        <Link to="/">Login or sign up</Link>
+      </p>
     </div>
-  );
+  );  
 }
 
 export default StoryPage;
